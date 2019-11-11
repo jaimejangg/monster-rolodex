7983eb634fc29d346a6e573334f3f439ea3f8b79
Jaime Rueda <jaime_rueda10@hotmail.com>
2019-11-11 17:52:42 -0600

    Updates

---
+++ precache-manifest.6b7b5e8286c33906e9d1ccc8bdc4b0fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a04ad3da16aa73293d287bc8fb282133",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9ca33ccdc7b1e5e3780e",
+    "url": "/monsters-rolodex/static/css/main.211bca61.chunk.css"
+  },
+  {
+    "revision": "b0b58fe607faa4d03a43",
+    "url": "/monsters-rolodex/static/js/2.de1aa811.chunk.js"
+  },
+  {
+    "revision": "9ca33ccdc7b1e5e3780e",
+    "url": "/monsters-rolodex/static/js/main.acf6ea00.chunk.js"
+  },
+  {
+    "revision": "1b1f57fd6c4b40aa49d9",
+    "url": "/monsters-rolodex/static/js/runtime-main.10abe5f5.js"
+  }
+]);
