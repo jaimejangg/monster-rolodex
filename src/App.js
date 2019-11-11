4e1d4e58380e3fbce5fc63cc6476d6b741449c72
Jaime Rueda <jaime_rueda10@hotmail.com>
2019-11-11 17:45:35 -0600

    add search and complete app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'; 
 
 import { CardList } from './components/card-list/card-list-component';
+import { SearchBox } from './components/search-box/SearchBox.component';
 
 import './App.css';
 
@@ -9,7 +10,8 @@ class App extends Component {
     super();
     
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -19,11 +21,24 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className='App'>
-        <CardList monsters={this.state.monsters} />
+        <h1>Monsters Roledex</h1>
+        <SearchBox 
+          placeholder='Search Monsters'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
---
+++ src/components/search-box/SearchBox.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './searchBox.styles.css';
+
+export const SearchBox = ({ placeholder, handleChange }) => (
+    <input
+        className='search'
+        type='search'
+        placeholder={placeholder}
+        onChange={handleChange}
+    />
+);
