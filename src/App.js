341f13cd5d37318c0b10fc94d796c6c6b3d71fa3
Jaime Rueda <jaime_rueda10@hotmail.com>
2019-11-11 17:50:25 -0600

    correciones

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
 
     return (
       <div className='App'>
-        <h1>Monsters Roledex</h1>
+        <h1>Monsters Rolodex</h1>
         <SearchBox 
           placeholder='Search Monsters'
           handleChange={this.handleChange}
